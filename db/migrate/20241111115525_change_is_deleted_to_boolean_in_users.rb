f455cdd6a6f81b8e91ca9e05c3604982ea4913dd
EC2 Default User <ec2-user@ip-172-31-39-189.ap-northeast-1.compute.internal>
2024-11-13 13:10:50 +0000

    migrateの不整合を修正

--- db/migrate/20241111115525_change_is_deleted_to_boolean_in_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangeIsDeletedToBooleanInUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :is_deleted, :boolean, default: false, null: false
-    remove_column :users, :is_deleted, :string
-  end
-end
